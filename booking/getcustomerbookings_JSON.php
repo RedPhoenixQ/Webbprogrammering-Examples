b200c83762f79c2c5f9450d74710b76e8fdfcd43
RedPhoenixQ <steve.dark.03@gmail.com>
2024-11-20 21:24:38 +0100

    Use json_encode for json output

--- booking/getcustomerbookings_JSON.php
+++ booking/getcustomerbookings_JSON.php
@@ -7,42 +7,20 @@ $type = getpostAJAX("type", true);
 reportMissingParams();
 
 try {
-  $querystring = "SELECT resource.type,booking.customerID,booking.resourceID,resource.name,resource.company,resource.location,DATE_FORMAT(booking.date,'%Y-%m-%d %H:%i') as date,DATE_FORMAT(booking.dateto,'%Y-%m-%d %H:%i') as dateto,booking.cost,booking.rebate,booking.position,booking.status,resource.category,resource.size,booking.auxdata FROM customer,booking,resource WHERE resource.ID=booking.resourceID AND booking.customerID=customer.ID AND customer.ID=:CUSTID AND type=:TYPE order by booking.date";
+  $querystring = "SELECT resource.type as application,booking.customerID,booking.resourceID,resource.name,resource.company,resource.location,DATE_FORMAT(booking.date,'%Y-%m-%d %H:%i') as date,DATE_FORMAT(booking.dateto,'%Y-%m-%d %H:%i') as dateto,booking.cost,booking.rebate,booking.position,booking.status,resource.category,resource.size,booking.auxdata FROM customer,booking,resource WHERE resource.ID=booking.resourceID AND booking.customerID=customer.ID AND customer.ID=:CUSTID AND type=:TYPE order by booking.date";
   $stmt = $pdo->prepare($querystring);
   $stmt->bindParam(':CUSTID', $customerID);
   $stmt->bindParam(':TYPE', $type);
   $stmt->execute();
 
-  header("Content-Type: application/json; charset=utf-8'");
-  $str = "[\n";
-  $cnt = 0;
-  foreach ($stmt as $key => $row) {
-    if ($cnt > 0)
-      $str .= ",";
-    $cnt++;
-    $str .= "{";
-    $key = $row['customerID'] . $row['resourceID'] . $row['position'] . $row['date'];
-    $key = str_replace(" ", "", $key);
-    $str .= '"key":"' . $key . '",';
-    $str .= '"application":"' . $row['type'] . '",';
-    $str .= '"customerID":"' . $row['customerID'] . '",';
-    $str .= '"resourceID":"' . $row['resourceID'] . '",';
-    $str .= '"name":"' . $row['name'] . '",';
-    $str .= '"company":"' . $row['company'] . '",';
-    $str .= '"location":"' . $row['location'] . '",';
-    $str .= '"date":"' . $row['date'] . '",';
-    $str .= '"dateto":"' . $row['dateto'] . '",';
-    $str .= '"position":"' . $row['position'] . '",';
-    $str .= '"cost":"' . $row['cost'] . '",';
-    $str .= '"category":"' . $row['category'] . '",';
-    $str .= '"size":"' . $row['size'] . '",';
-    $str .= '"auxdata":"' . $row['auxdata'] . '"';
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $str .= "}\n";
+  foreach ($result as &$row) {
+    $row["key"] = $row['customerID'] . $row['resourceID'] . $row['position'] . $row['date'];
   }
-  $str .= "]\n";
-  echo $str;
 
+  header("Content-Type: application/json; charset=utf-8'");
+  echo json_encode($result);
 } catch (PDOException $e) {
   err("Error!: " . $e->getMessage() . "<br/>");
   die();
